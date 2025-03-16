875bb6e3a81e44faef20563b76168ae4a98017cb
Mike Schinkel <mike@newclarity.net>
2025-03-16 00:58:40 -0400

    Add comments to `open_tag_type.go`

--- classifier/open_tag_type.go
+++ classifier/open_tag_type.go
@@ -1,13 +1,24 @@
 package classifier
 
+// OpenTagType represents the type of opening sequence for a tag in a Mustache
+// template. It distinguishes between standard delimiters (default: '{{') and
+// triple braces ('{{{').
 type OpenTagType int8
 
 const (
+	// InvalidOpenTag represents an invalid or unrecognized opening tag sequence.
 	InvalidOpenTag OpenTagType = iota
+
+	// TripleBraceOpen represents the triple-brace opening sequence '{{{' for
+	// unescaped variables.
 	TripleBraceOpen
+
+	// DelimiterOpen represents the standard opening delimiter (default: '{{') for
+	// typical tags.
 	DelimiterOpen
 )
 
+// String returns a human-readable representation of OpenTagType for error messages.
 func (ott OpenTagType) String() string {
 	switch ott {
 	case TripleBraceOpen:
